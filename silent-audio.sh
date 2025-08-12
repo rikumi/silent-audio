896bb13e288a7f05e6988ede1ab27887a20e1a1b
Rikumi <rikumi.dev@gmail.com>
2025-08-13 00:33:51 +0800

    Update silent-audio.sh

--- silent-audio.sh
+++ silent-audio.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 while true; do
+    # loop another audio asynchronously with 1 second delay to prevent speaker dying between the gaps
+	sh -c "sleep 1; aplay $(dirname $0)/silent.wav" &
 	aplay "$(dirname $0)/silent.wav"
 done
